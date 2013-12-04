ba835acf0e21d460edc01017599d5db6148082c2
Shummy42 <yaugen.shumilau@gmail.com>
2013-12-05 01:39:47 +0300

    Hello, World!

---
+++ lab2/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import socket
+from sys import argv
+
+size = 128
+ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+ServerSocket.bind((argv[1],int(argv[2])));
+ServerSocket.listen(1);
+try:
+	while True:
+		client, address = ServerSocket.accept();
+		while True:
+			data = client.recv(size)
+			if data:
+				client.send(data.lower());
+			else:
+				break;
+finally:
+	client.close();
