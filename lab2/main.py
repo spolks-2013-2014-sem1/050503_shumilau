47589e4346b41e0947e3bebb175377341fd01cce
Shummy42 <yaugen.shumilau@gmail.com>
2013-12-05 03:35:42 +0300

    Arguments not necessary now.

--- lab2/main.py
+++ lab2/main.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python
 import socket
-from sys import argv
+import sys
+import getopt
 
-size = 128;
+ADDRESS = ""
+PORT = 1337
+SIZE = 512
+
+try:
+   opts, args = getopt.getopt(sys.argv[1:],"h:a:p:",["address=","port="]);
+except getopt.GetoptError:
+   print("main.py -a <address> -p <port>");
+   sys.exit(2);
+for opt, arg in opts:
+   if opt == '-h':
+      print("main.py -a <address> -p <port>");
+      sys.exit();
+   elif opt in ("-a", "--address"):
+      ADDRESS = arg;
+   elif opt in ("-p", "--port"):
+      PORT = int(arg);
+
 ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-ServerSocket.bind((argv[1],int(argv[2])));
+ServerSocket.bind((ADDRESS,PORT));
 ServerSocket.listen(1);
 try:
-	while True:
-		client, address = ServerSocket.accept();
-		while True:
-			data = client.recv(size);
-			if data:
-				client.send(data.lower());
-			else:
-				break;
+   while True:
+      client, address = ServerSocket.accept();
+      while True:
+         data = client.recv(SIZE);
+         if data:
+            client.send(data.lower());
+         else:
+            break;
 finally:
-	client.close();
+   client.close();
