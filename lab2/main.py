677a7dcb22fb3a2a7dbf2fd2a5ff50d9b5e88f18
Shummy42 <yaugen.shumilau@gmail.com>
2013-12-05 07:52:55 +0300

    Added handler of exception.

--- lab2/main.py
+++ lab2/main.py
@@ -21,8 +21,13 @@ for opt, arg in opts:
    elif opt in ("-p", "--port"):
       PORT = int(arg);
 
-ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-ServerSocket.bind((ADDRESS,PORT));
+try:
+   ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+   ServerSocket.bind((ADDRESS,PORT));
+except socket.error as msg:
+    print("Failed to create a socket. Error #{0}: {1}".format(msg.errno, msg.strerror))
+    sys.exit(3)
+
 ServerSocket.listen(1);
 try:
    client, address = ServerSocket.accept();
